fe2b4485e9968cf773d7246cd7328f923349428b
Marvin Ren <marvin.ren@yale.edu>
2021-10-27 23:26:46 -0400

    update

---
+++ db/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE candidates (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    idustry_connected BOOLEAN NOT NULL,
+)
