4a95efb30abcef0e7099b7359864997a726577b1
Marvin Ren <marvin.ren@yale.edu>
2021-10-28 02:02:17 -0400

    user

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const mysql = require('mysql2');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 //Express middleware
 app.use(express.urlencoded({extend: false}));
 app.use(express.json());
+//Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your mySql username,
+        user: ''
+    }
+)
 
 
 //default response for any other request (not found)
