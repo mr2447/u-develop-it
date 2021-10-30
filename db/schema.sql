c42b9659577470b07db78ef66cd259635c77e93b
Marvin Ren <marvin.ren@yale.edu>
2021-10-30 16:04:42 -0400

    error

--- db/schema.sql
+++ db/schema.sql
@@ -12,6 +12,6 @@ CREATE TABLE candidates (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     party_id INTEGER,
-    industry_connected BOOLEAN NOT NULL
+    industry_connected BOOLEAN NOT NULL,
     CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
